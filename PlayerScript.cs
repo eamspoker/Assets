3089fc212376de14e3df1bcf9507c21b2b0501fa
Jiahe Tian <jiahet@andrew.cmu.edu>
2023-02-04 12:27:20 -0500

    conflic handling

--- PlayerScript.cs
+++ PlayerScript.cs
@@ -18,14 +18,14 @@ public class PlayerScript : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         manager = GameManager.GetComponent<GameManagerScript>();
-        
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        ItemInteraction();
+        ShowGameManagerStatus();
         lastVelocity = rb.velocity;
 
     }
@@ -35,40 +35,105 @@ public class PlayerScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D coll)
     {
 
-        
-        if(coll.collider == true)
+
+        if (coll.collider == true)
         {
-        if(coll.collider.gameObject.tag == "Door")
-        {
-            transform.position = PlayerSpawn.transform.position;
-            manager.isTransitioning = true;
+            if (coll.collider.gameObject.tag == "Door")
+            {
+                transform.position = PlayerSpawn.transform.position;
+                manager.isTransitioning = true;
+            }
+            Debug.Log("Touching game object: " + coll.gameObject.name);
+            manager.touchingObj = coll.gameObject;
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.collider == true)
+        {
+            manager.touchingObj = null;
         }
     }
 
 
     void Move()
     {
-       Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
 
-       if (move.Equals(Vector3.zero))
-       {
-        // return to walking speed once not moving
-        increasedSpeed = walkSpeed;
-
-       } else {
-        // increase speed while walking
-        float potentialInc = increasedSpeed + 0.1f*Time.deltaTime;
-        if (potentialInc > maxSpeed)
+        if (move.Equals(Vector3.zero))
         {
-            increasedSpeed = maxSpeed;
-        } else {
-            increasedSpeed = potentialInc;
-        }
-       }
+            // return to walking speed once not moving
+            increasedSpeed = walkSpeed;
 
+        }
+        else
+        {
+            // increase speed while walking
+            float potentialInc = increasedSpeed + 10f * Time.deltaTime;
+            if (potentialInc > maxSpeed)
+            {
+                increasedSpeed = maxSpeed;
+            }
+            else
+            {
+                increasedSpeed = potentialInc;
+            }
+        }
+
+        //transform.position += move * increasedSpeed * Time.deltaTime;
+        Rigidbody2D comp = GetComponent<Rigidbody2D>();
+        comp.AddForce(move * increasedSpeed * Time.deltaTime);
+
+<<<<<<< HEAD
        velocity = move * increasedSpeed;
        rb.velocity = velocity;
     }
 }
+=======
+    }
+    #region item interaction
+    void ItemInteraction()
+    {
+        // TODO: check whether the touching object is allowed to be interacted with
+        if (Input.GetKey("e") && manager.touchingObj != null)
+        {
+            Debug.Log("Interacting with the item: " + manager.touchingObj.name);
+            AddItemToInventory(manager.touchingObj);
+            Destroy(manager.touchingObj);
+            manager.touchingObj = null;
+        }
+    }
+
+    void AddItemToInventory(GameObject obj)
+    {
+        if (!manager.inventory.ContainsKey(obj.name))
+        {
+            manager.inventory[obj.name] = 0;
+        }
+        manager.inventory[obj.name]++;
+        PrintGameManagerStatus();
+    }
+    #endregion
+
+    #region display game manager status
+    void PrintGameManagerStatus()
+    {
+        Debug.Log("Inventory:");
+        foreach (KeyValuePair<string, int> kvp in manager.inventory)
+        {
+            Debug.Log(string.Format("Item: {0}, Count: {1}", kvp.Key, kvp.Value));
+        }
+    }
+
+    void ShowGameManagerStatus()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            PrintGameManagerStatus();
+        }
+    }
+    #endregion
+}
+>>>>>>> 33c420e (Inventory)
